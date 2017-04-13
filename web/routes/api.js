b22fb1150e584ca41cb8ddb015ab52f30cc1b36a
soraya2 <soraya_ibrahim@live.nl>
2017-04-13 15:13:53 +0200

    added dotenv

--- web/routes/api.js
+++ web/routes/api.js
@@ -6,4 +6,32 @@ router.get('/', function(req, res, next) {
 });
 
 
+load(callback, url); //calls function load, gives the query and calls function callback
+function callback(data) {
+  var temp = data.main.temp;
+  var speed = data.wind.speed;
+
+  console.log(temp, speed);
+
+  function computeFactor(t, s) {
+  var windchill = calculateChill(temp, speed);
+    if(windchill >= 20) {
+      console.log('Korte broeken weer');
+      // kleur groen naar node mcu
+    } else {
+      console.log('Helaas');
+      // kleur rood naar node mcu
+    }
+  return windchill.toFixed(2);
+ }
+
+ function calculateChill(tempF, speed) {
+  var chill = (35.74 + (.6215 * tempF)) - (35.75 * Math.pow(speed, .16)) + (.4275 * (tempF * Math.pow(speed, .16)));
+    var celsius = (chill - 32) * 5 / 9;
+  return celsius;
+ }
+  console.log("Het voelt als " + computeFactor(temp, speed) + " graden celsius");
+}
+
+
 module.exports = router;
