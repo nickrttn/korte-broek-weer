3cc0d00016a6cef951497789cbc5c656209127d1
soraya2 <soraya_ibrahim@live.nl>
2017-04-19 19:46:42 +0200

    added api and website

---
+++ website/routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+
+var http = require('http');
+var request = require('request');
+var router = express.Router();
+var env = require( 'dotenv' ).config();
+
+//render the homepage
+router.get('/', function(req, res) {
+  res.render('index', { title: 'welkom' });
+});
+
+//post request to api server
+router.post('/', function(req, res) {
+
+request.post({
+ url: process.env.ENDPOINT,
+ form: {
+  id: req.body.boxid.value,
+  name: req.body.name.value,
+  color: req.body.color.value
+ },
+ json: true
+}, function(err, response, body) {
+    res.send('Succes');
+ });
+});
+
+module.exports = router;
+
