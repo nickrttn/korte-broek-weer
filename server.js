14c35855f9d8a74e92bf75a3c646c2d2984d183d
Nick Rutten <hello@nickrttn.me>
2017-04-20 11:09:26 +0200

    refactor some stuff

--- server.js
+++ server.js
@@ -1,27 +1,20 @@
+var path = require('path');
+var http = require('http');
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var path = require('path');
-var http = require('http');
+var api = require('./routes/api');
 
 require('dotenv').config();
 
 var app = express();
-var server = http.createServer(app);
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
-
-app.set('views', path.join(__dirname, 'views'));
-
-var api = require('./routes/api');
-
-app.use('/api', api);
 
 app.set('port', process.env.PORT);
 
-server.listen(app.get('port'), function(){
-    console.log('app started on localhost:3005');
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', api);
 
+app.listen(app.get('port'), function(){
+  console.log(`---- listening on http://localhost:${app.get('port')}`);
+});
