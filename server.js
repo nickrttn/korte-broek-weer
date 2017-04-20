479ef94f8ac6ed2f2e21ddd54d8608a745cac042
Nick Rutten <hello@nickrttn.me>
2017-04-20 13:18:53 +0200

    refactor API and add windchill long polling

--- server.js
+++ server.js
@@ -1,31 +1,25 @@
+var path = require('path');
+var http = require('http');
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var path = require('path');
-var http = require('http');
+var index = require('./routes/index');
 
 require('dotenv').config();
 
 var app = express();
-var server = http.createServer(app);
+
+app.set('port', process.env.PORT);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
-
-app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.static(path.join(__dirname, 'public')));
-
-var index = require('./routes/index');
-
-
 app.use('/', index);
-
 app.use('/user/add', index);
 
-app.set('port', process.env.PORT);
-
-server.listen(app.get('port'), function(){
-    console.log('app started on localhost:3006');
+app.listen(app.get('port'), function(err) {
+	if (err) throw err;
+  console.log(`app started on localhost:${app.get('port')}`);
 });
